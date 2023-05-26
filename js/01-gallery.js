87875cc08c0d5977da1c9273808a1531648b516a
Artur Rakhimov <kharkiv1c@gmail.com>
2023-05-26 23:01:23 +0200

    dev

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gal = document.querySelector(".gallery");
+galleryItems.forEach((element) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<li class="gallery__item">
+                        <img
+                            class="gallery__image"
+                            src="${element.preview}"
+                            alt="${element.description}"
+                        />
+                    </li>`;
+    gal.appendChild(li);
+
+});
 
 
 console.log(galleryItems);
