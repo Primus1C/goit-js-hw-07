e81b085a3a356eff20e09f130024685dd56d2b2d
Artur <110535829+Primus1C@users.noreply.github.com>
2023-05-29 00:35:49 +0200

    dev

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ gallery.addEventListener("click", onClickGallery);
 galleryItems.forEach((element) => {
     const li = document.createElement("li");
     li.innerHTML = `<li class="gallery__item">
-                        <a class="gallery__link" href="large-image.jpg">
+                        <a class="gallery__link" href="${element.original}">
                             <img
                             class="gallery__image"
                             src="${element.preview}"
