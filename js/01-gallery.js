aa6e666ab7dfd23e958d9d9e2d57512ab24ca4f2
Artur <110535829+Primus1C@users.noreply.github.com>
2023-05-29 10:57:11 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,40 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const currentLightbox = [0];
 const gallery = document.querySelector(".gallery");
 gallery.addEventListener("click", onClickGallery);
 
 galleryItems.forEach((element) => {
     const li = document.createElement("li");
-    li.innerHTML = `
-                        <a class="gallery__link" href="${element.original}">
-                            <img
+    // href="${element.original}"
+    li.innerHTML = `<a class="gallery__link">
+                        <img
                             class="gallery__image"
                             src="${element.preview}"
                             data-source="${element.original}"
                             alt="${element.description}"
-                            />
-                        </a>`;
+                        />
+                    </a>`;
     li.className = "gallery__item";
     gallery.append(li);
 });
 
 function onClickGallery(evt) {
-   // console.log(evt.target.nodeName);
-    console.log(evt.target);
+    
+    if (evt.target.nodeName != "IMG") {
+        return;
+    }
 
+    const lightbox = basicLightbox.create(`<img width="1400" height="900" src=${evt.target.dataset.source}>`);
+    currentLightbox.splice(0, 1, lightbox);
+    lightbox.show();
+    return false;
 }
 
-//console.log(galleryItems);
+document.addEventListener("keydown", (e) => {
+    if (e.code == "Escape") {
+        currentLightbox[0].close();
+    }
+});
+
